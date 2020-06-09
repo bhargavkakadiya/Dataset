84b3eb5e85bc15f3d404bc153b87759d652d710d
Ubuntu <AzureUser@ABK.aib5kxyh451e5lmdqq1udqr0gd.bx.internal.cloudapp.net>
2020-06-09 01:35:57 +0000

    part2 assignment

--- t1.sh
+++ t1.sh
@@ -3,4 +3,10 @@ import pandas as pd
 import numpy as np
 data = pd.read_csv('2007.csv')
 a1 = data["Dest"].value_counts()
+print("Part A Answer\n")
 print(a1.head(3))
+
+print("Part B Answer\n")
+
+a2 = data[data["Origin"]=="SFO"].head(3)
+print(a2["ArrDelay"])
